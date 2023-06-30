89ed670c6f1bc7107b5204d4af927669d7ea7f22
Kayle Maximus <kaylemaximus@gmail.com>
2023-07-01 01:11:03 +0700

    Finish level start evaluate

--- Project Boost/Assets/Script/Model/LevelData.cs
+++ Project Boost/Assets/Script/Model/LevelData.cs
@@ -10,6 +10,16 @@ public class LevelData : MonoBehaviour
     public float time;
     public bool active;
 
+    [SerializeField]
+    private Sprite[] _startImage;
+    [SerializeField]
+    private Image _liveImage;
+
+    private void Start()
+    {
+        _liveImage = GetComponent<Transform>().GetChild(1).GetComponent<Image>();
+    }
+
     public void AssignLevelValue(string name, bool isActive, float finishTime)
     {
         sceneName = name;
@@ -21,11 +31,33 @@ public class LevelData : MonoBehaviour
     {
         GetComponent<Button>().onClick.AddListener(LoadLevel);
         GetComponent<Button>().interactable = active;
+        EvaluateLevel();
     }
 
     private void LoadLevel()
     {
         GameObject.Find("LevelLoader").GetComponent<SceneLoader>().LoadSelectedLevel(sceneName);
+    }
+
+    private void EvaluateLevel()
+    {
+        if (time == 0)
+        {
+            GetComponent<Transform>().GetChild(1).GetComponent<Image>().sprite = _startImage[0];
+        }
+        else
+        if (time <= 30)
+        {
+            GetComponent<Transform>().GetChild(1).GetComponent<Image>().sprite = _startImage[3];
+        }
+        else if (time <= 60f && time > 30)
+        {
+            GetComponent<Transform>().GetChild(1).GetComponent<Image>().sprite = _startImage[2];
+        }
+        else
+        {
+            GetComponent<Transform>().GetChild(1).GetComponent<Image>().sprite = _startImage[1];
+        }
     }
 
 }
